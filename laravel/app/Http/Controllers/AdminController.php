a0a5fbd9ff2c8e5bef7c157b4d44278bbe05588a
thuytt.hus <thuytt.hus@gmail.com>
2020-10-28 23:32:07 +0700

    first commit

---
+++ laravel/app/Http/Controllers/AdminController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\AdminModel;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin')->only('index');
+    }
+
+    public function index(){
+        return view('admin.dashboard');
+    }
+
+    public function create(){
+        return view('admin.auth.register');
+    }
+
+    public function store(Request $request){
+        $this->validate($request, array(
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ));
+
+        // Khởi tạo model để lưu admin mới
+        $adminModel = new AdminModel();
+        $adminModel->name = $request->name;
+        $adminModel->email = $request->email;
+        $adminModel->password = bcrypt($request->password);
+        $adminModel->save();
+
+        return redirect()->route('admin.auth.login');
+    }
+
+}
---
+++ laravel/resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,52 @@
+<div class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+    <!--left-fixed -navigation-->
+    <aside class="sidebar-left">
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <h1><a class="navbar-brand" href="{{ url('/') }}"><span class="fa fa-area-chart"></span> Glance<span class="dashboard_text">Design dashboard</span></a></h1>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="sidebar-menu">
+                    <li class="header">MAIN NAVIGATION</li>
+                    <li class="treeview">
+                        <a href="{{ url('/admin/') }}">
+                            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-laptop"></i>
+                            <span>Nhà tuyển dụng</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{ url('/admin/parner') }}"><i class="fa fa-angle-right"></i> Danh mục</a></li>
+                            <li><a href="{{ url('/admin/shop/product') }}"><i class="fa fa-angle-right"></i> Sản phẩm</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-laptop"></i>
+                            <span>Học giả</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{ url('/admin/content/category') }}"><i class="fa fa-angle-right"></i> Danh mục</a></li>
+                            <li><a href="{{ url('/admin/content/post') }}"><i class="fa fa-angle-right"></i> Bài viết</a></li>
+                        </ul>
+                    </li>
+
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+    </aside>
+</div>
